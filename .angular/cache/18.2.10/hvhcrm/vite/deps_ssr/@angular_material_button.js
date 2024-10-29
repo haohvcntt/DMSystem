e7475fdf32fe9e104ccea3baa02351d68b41ce6e
haohv <haohv.cntt@gmail.com>
2024-10-29 11:29:57 +0700

    init project

---
+++ .angular/cache/18.2.10/hvhcrm/vite/deps_ssr/@angular_material_button.js
@@ -0,0 +1,35 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-FDBAGNOB.js";
+import "./chunk-2UU7Q3SW.js";
+import "./chunk-DQZ2PILJ.js";
+import "./chunk-TKQWSO4T.js";
+import "./chunk-2H3L6IVL.js";
+import "./chunk-NQ4HTGF6.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
